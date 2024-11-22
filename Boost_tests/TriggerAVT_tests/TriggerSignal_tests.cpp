e23c8fd7709a460ada2a9b74f7dfe13bb8824122
spect0x4552 <valdisreturns@proton.me>
2024-11-22 04:26:32 +0200

    Test suite init

--- Boost_tests/TriggerAVT_tests/TriggerSignal_tests.cpp
+++ Boost_tests/TriggerAVT_tests/TriggerSignal_tests.cpp
@@ -3,8 +3,32 @@
 #define BOOST_TEST_MODULE Camera
 #include <boost/test/unit_test.hpp>
 
-#include "CamControl_helper.hpp"
-#include "GPIO_helper.hpp"
-#include "I2C_helper.hpp"
+#include "CamControl_mockHelper.hpp"
+#include "GPIO_mockHelper.hpp"
+#include "I2C_mockHelper.hpp"
 
-int main(int argc, char* argv[]); 
+/* 
+    Test fixture for camera control
+*/
+struct CamControlFixture {
+    CamControl_mockHelper camc_helper; 
+    GPIO_mockHelper gpio_helper; 
+    I2C_mockHelper i2c_helper; 
+
+    CamControlFixture() : camControl(gpio_helper, i2c_helper) {
+        gpio_helper.init(); 
+        i2c_helper.init(); 
+    }
+
+    ~CamControlFixture() {
+        gpio_helper.cleanup();
+        i2c_helper.cleanup(); 
+    }
+}; 
+
+/* 
+    TEST SUITE BEGIN
+*/
+
+
+
